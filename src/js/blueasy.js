c8f0656e82adffa633c13efb7424e072a596157c
fair-man <ruslan.tukalenko@gmail.com>
2016-02-25 18:38:33 +0300

    add js header navigation

--- src/js/blueasy.js
+++ src/js/blueasy.js
@@ -1,19 +1,29 @@
-var $header = $(".header");
+var $header = $('.header');
 
 $(window).scroll(function(){
     if ( $(this).scrollTop() > 660){
-        $header.addClass("menu-grey");
+        $header.addClass('menu-grey');
     } else if($(this).scrollTop() <= 660) {
-        $header.removeClass("menu-grey");
+        $header.removeClass('menu-grey');
     }
 });
 
-$('.js-dropdown__link').on('click', function(e) {
+var $dropdownLink = $('.js-dropdown__link');
+
+$dropdownLink.on('click', function(e) {
     e.preventDefault();
     $('.portfolio-block__list').toggleClass('portfolio-block__list-open');
 });
 
 $('.js-dropdown__list').on('click','.list__item-link', function(e) {
    var $elem = $(this);
-   $('.js-dropdown__link').html($elem.html());
+    $dropdownLink.html($elem.html() + '<b class="caret"></b>');
+   $('.portfolio-block__list').removeClass('portfolio-block__list-open');
+});
+
+$('.js-nav__list').on('click', '.js-list-item__link', function(e) {
+    e.preventDefault();
+    var $attr = $(this).attr('href');
+    console.log($attr);
+
 });
